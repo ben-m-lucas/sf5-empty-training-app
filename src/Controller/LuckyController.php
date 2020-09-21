f2324525124ada0f4bf421c6357db49dab1fb255
Ben Lucas <blucas@datto.com>
2020-09-21 09:39:09 -0700

    use annotations

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class LuckyController
 {
+    /**
+     * @Route("/lucky/number/{maxNumber<\d+>?100}", methods={"GET"})
+     */
     public function number($maxNumber): Response
     {
         $number = random_int(0, $maxNumber);
@@ -16,6 +20,9 @@ class LuckyController
         );
     }
 
+    /**
+     * @Route("/api/lucky/number/{maxNumber<\d+>?100}", methods={"GET"})
+     */
     public function numberApi($maxNumber): Response
     {
         $number = random_int(0, $maxNumber);
